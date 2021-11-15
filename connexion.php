ba728adb7f0f2a5c4703042dcad9e05cc336ab79
beinjamin <jauresbeinjamin@gmail.com>
2021-11-15 21:27:48 +0100

    Suppression du contenue index

--- connexion.php
+++ connexion.php
@@ -4,4 +4,8 @@ require_once 'config.php';
 
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
+
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+    $check = $bdd->prepare('SELECT pseudo, email ,password FROM utilisateurs WHERE email = ?');
 } else header('Location:index.php');
