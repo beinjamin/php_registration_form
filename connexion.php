3acdbee3d0782080435a7957949d80b9d00eac59
beinjamin <jauresbeinjamin@gmail.com>
2021-11-20 18:11:03 +0100

    Stoker les donnee dans data verification si lesparent existe dans la table

--- connexion.php
+++ connexion.php
@@ -8,4 +8,6 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = htmlspecialchars($_POST['email']);
     $password = htmlspecialchars($_POST['password']);
     $check = $bdd->prepare('SELECT pseudo, email ,password FROM utilisateurs WHERE email = ?');
+    $check->execute(array($email,));
+    $data = $check->fetch();
 } else header('Location:index.php');
